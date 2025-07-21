92a99618f849996496fbd81fdcd8629a5b0543fb
Swetha <Swethalakshmi64@gmail.com>
2025-07-21 22:42:35 +0200

    reorganise server file

--- server.ts
+++ server.ts
@@ -3,32 +3,34 @@ import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 
+// constants
 const MONGO_URI = "mongodb+srv://swethalakshmideepak:S%40Weth29GR-EaT@expense-tracker.ah3wcum.mongodb.net/";
 const PORT = 3000;
+
+// Server Setup
+interface ServerConnectErr extends Error {
+  code?: string | number;
+  status?: number;
+}
+
 const app = express();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}).on('error', (err: ServerConnectErr) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(`----- Port ${PORT} is busy -----`);
+    } else {
+      console.log({ err }, err.code);
+    }
+});
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
 // app.use("/api", router);
-interface ServerConnectErr {
-  code: String
-}
 
-function appStart (curPort = PORT) {
-  app.listen(curPort, () => {
-    console.log(`Server is running on http://localhost:${curPort}`);
-  }).on('error', (err: ServerConnectErr) => {
-
-    if (err.code === 'EADDRINUSE') {
-      console.log(`----- Port ${curPort} is busy, trying with port ${curPort + 1} -----`);
-      appStart(curPort + 1)
-    }
-    console.log({ err }, err.code);
-  });
-}
-
-appStart(PORT);
+// MongoDB Setup
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI).then(() => {
   console.log('Connected to MongoDB');
