69f322be9c693fc586418506b67ad02aa3dff019
Swetha <Swethalakshmi64@gmail.com>
2025-07-23 12:02:31 +0200

    removed mongo uri

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # expense_tracker
+
+TO DO
+1. Logger - winston
+2. Swagger Integration
--- utils/Constants.ts
+++ utils/Constants.ts
@@ -1,11 +1,13 @@
-const MONGO_URI = "mongodb+srv://swethalakshmideepak:S%40Weth29GR-EaT@expense-tracker.ah3wcum.mongodb.net/";
+import Joi from "joi";
+
 const PORT = 3000;
 const SALT_VALUE = 10;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const OBJECT_ID = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
 
 export {
-  MONGO_URI,
   PORT,
   SALT_VALUE,
   EMAIL_REGEX,
+  OBJECT_ID,
 }
